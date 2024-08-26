111f6272b63c7d2299212c141b24d93a53ffae5a
Francisco Soares <fmssn@cin.ufpe.br>
2024-08-26 10:21:53 -0300

    Initial Commit

---
+++ ChallengeTribos/ChallengeTribosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeTribosApp.swift
+//  ChallengeTribos
+//
+//  Created by Francisco Miranda Soares on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeTribosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChallengeTribos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChallengeTribos
+//
+//  Created by Francisco Miranda Soares on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
